0f3d72f6591abce84c8f9bcb92d8dd89f8dd4b25
codebuilder2022 <kumarankaushik@gmail.com>
2022-08-24 14:34:26 +0530

    worked

--- index.ts
+++ index.ts
@@ -8,6 +8,10 @@ connectDB()
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req:Request, res: Response, next:NextFunction) => {
+  res.json('worked')
+})
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
   });
